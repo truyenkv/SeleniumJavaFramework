861de82bced037b16848c32c878be97c672ff06a
Dang <kieuvantruyen.3dfish@gmail.com>
2020-09-17 05:53:08 +0700

    Implementing js

--- actions/common/AbstractPage.java
+++ actions/common/AbstractPage.java
@@ -5,8 +5,11 @@ import java.util.Set;
 
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -14,9 +17,14 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public abstract class AbstractPage {
 
 	private Alert alert;
-	WebDriverWait explicitWait;
+	private WebDriverWait explicitWait;
 	private WebElement element;
-	Select select;
+	private Select select;
+	private JavascriptExecutor jsExecutor;
+	private long longTimeOut = 30;
+	private short shortimeOut = 10;
+	private List<WebElement> elements;
+	private Actions action;
 
 	public void openUrl(WebDriver driver, String url) {
 		driver.get(url);
@@ -66,15 +74,15 @@ public abstract class AbstractPage {
 		alert.sendKeys(keyword);
 	}
 
-	public void waitAlertPresence(WebDriver driver, int time) {
-		explicitWait = new WebDriverWait(driver, time);
+	public void waitAlertPresence(WebDriver driver) {
+		explicitWait = new WebDriverWait(driver, shortimeOut);
 		explicitWait.until(ExpectedConditions.alertIsPresent());
 	}
 
 	public void switchWindowByID(WebDriver driver, String idswitch) {
 		Set<String> ids = driver.getWindowHandles();
-		for(String id:ids) {
-			if(id.equals(idswitch)) {
+		for (String id : ids) {
+			if (id.equals(idswitch)) {
 				driver.switchTo().window(id);
 				break;
 			}
@@ -83,67 +91,174 @@ public abstract class AbstractPage {
 
 	public void switchWindowByTitle(WebDriver driver, String title) {
 		Set<String> ids = driver.getWindowHandles();
-		for(String id:ids) {
-				driver.switchTo().window(id);
-				if(driver.getTitle().equals(title)) {
-					break;
-				}
+		for (String id : ids) {
+			driver.switchTo().window(id);
+			if (driver.getTitle().equals(title)) {
+				break;
+			}
 		}
 	}
-	
+
 	public void closeAllWindowWithoutParent(WebDriver driver, String parentId) {
 		Set<String> ids = driver.getWindowHandles();
-		for(String id:ids) {
-			if(!id.equals(parentId)) {
+		for (String id : ids) {
+			if (!id.equals(parentId)) {
 				driver.switchTo().window(id);
 				driver.close();
 			}
 		}
 		driver.switchTo().window(parentId);
 	}
-	
-	//return element
+
+	// return element
 	public WebElement find(WebDriver driver, String xpathElement) {
 		return driver.findElement(byXpath(xpathElement));
 	}
-	
-	//return list element
-	public List<WebElement> finds(WebDriver driver, String xpathElement){
+
+	// return list element
+	public List<WebElement> finds(WebDriver driver, String xpathElement) {
 		return driver.findElements(byXpath(xpathElement));
 	}
-	
-	//return by with xpath
+
+	// return by with xpath
 	public By byXpath(String xpathElement) {
 		return By.xpath(xpathElement);
 	}
-	
+
 	public void clickToElement(WebDriver driver, String xpathElement) {
-		find(driver, xpathElement).click();;
+		find(driver, xpathElement).click();
 	}
-	
-	
-	
+
 	public void sendKeyToField(WebDriver driver, String xpathElement, String text) {
 		element = find(driver, xpathElement);
 		element.clear();
 		element.sendKeys(text);
 	}
-	
+
 	public void selectItemDropdownByText(WebDriver driver, String xpathElement, String valueSelect) {
 		select = new Select(find(driver, xpathElement));
 		select.selectByVisibleText(valueSelect);
 	}
-	
+
 	public String getSelectedItemDropdown(WebDriver driver, String xpathElement) {
 		select = new Select(find(driver, xpathElement));
 		return select.getFirstSelectedOption().getText();
 	}
-	
+
 	public boolean isDropdownMultiple(WebDriver driver, String xpathElement) {
 		select = new Select(find(driver, xpathElement));
 		return select.isMultiple();
 	}
+
+	public void sleepInSecond(int sec) {
+		try {
+			Thread.sleep(sec * 1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void selectItemCustomDropdown(WebDriver driver, String xpathParent, String childLocator, String expectedItem) {
+		// click parent
+		find(driver, xpathParent).click();
+		explicitWait = new WebDriverWait(driver, longTimeOut);
+		explicitWait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath(childLocator)));
+		elements = finds(driver, childLocator);
+		for (WebElement element : elements) {
+			if (element.getText().equals(expectedItem)) {
+				jsExecutor = (JavascriptExecutor) driver;
+				jsExecutor.executeScript("arguments[0].scrollIntoView(true);", element);
+				sleepInSecond(2);
+				element.click();
+				break;
+			}
+		}
+
+	}
+
+	public String getElementAttribute(WebDriver driver, String xpathElement, String attribute) {
+		return find(driver, xpathElement).getAttribute(attribute);
+	}
+
+	public String getElementText(WebDriver driver, String xpathElement) {
+		return find(driver, xpathElement).getText();
+	}
+
+	public int countElementNumber(WebDriver driver, String xpathElement) {
+		return finds(driver, xpathElement).size();
+	}
+
+	public void checkTheCheckBox(WebDriver driver, String xpathElement) {
+		if(!find(driver, xpathElement).isSelected()) {
+			find(driver, xpathElement).click();
+		}
+	}
+
+	public void uncheckTheCheckBox(WebDriver driver, String xpathElement) {
+		if(find(driver, xpathElement).isSelected()) {
+			find(driver, xpathElement).click();
+		}
+	}
 	
+	public boolean isElementDisplay(WebDriver driver, String xpathElement) {
+		return find(driver, xpathElement).isDisplayed();
+	}
 	
+	public boolean isElementSelect(WebDriver driver, String xpathElement) {
+		return find(driver, xpathElement).isSelected();
+	}
+	
+	public boolean isElementEnable(WebDriver driver, String xpathElement) {
+		return find(driver, xpathElement).isEnabled();
+	}
+	
+	public void switchToFrame(WebDriver driver, String xpathElement){
+		driver.switchTo().frame(find(driver, xpathElement));
+	}
+	
+	public void switchToDefault(WebDriver driver) {
+		driver.switchTo().defaultContent();
+	}
+	
+	public void hoverMouseToElement(WebDriver driver, String xpathElement) {
+		action = new Actions(driver);
+		action.moveToElement(find(driver, xpathElement)).perform();
+	}
+	
+	public void sendKeyToElement(WebDriver driver, String xpathElement, Keys key) {
+		action = new Actions(driver);
+		action.sendKeys(find(driver, xpathElement), key);
+	}
+	
+	public void highlightElement(WebDriver driver, String xpathElement) {
+		jsExecutor = (JavascriptExecutor) driver;
+		WebElement element = find(driver, xpathElement);
+		String originalStyle = element.getAttribute("style");
+		jsExecutor.executeScript("arguments[0].setAttribute(arguments[1], arguments[2])", element, "style", "border: 2px solid red; border-style: dashed;");
+		sleepInSecond(1);
+		jsExecutor.executeScript("arguments[0].setAttribute(arguments[1], arguments[2])", element, "style", originalStyle);
+
+	}
+	
+	public void clickToElementByJS(WebDriver driver, String xpathElement) {
+		jsExecutor = (JavascriptExecutor) driver;
+		jsExecutor.executeScript("arguments[0].click();", find(driver, xpathElement));
+	}
+
+	public void scrollToElement(WebDriver driver, String xpathElement) {
+		jsExecutor = (JavascriptExecutor) driver;
+		jsExecutor.executeScript("arguments[0].scrollIntoView(true);", find(driver, xpathElement));
+	}
+	
+	public void sendkeyToElementByJS(WebDriver driver, String xpathElement, String value) {
+		jsExecutor = (JavascriptExecutor) driver;
+		jsExecutor.executeScript("arguments[0].setAttribute('value', '" + value + "')", find(driver, xpathElement));
+	}
+
+	public void removeAttributeInDOM(WebDriver driver, String xpathElement, String attributeRemove) {
+		jsExecutor = (JavascriptExecutor) driver;
+		jsExecutor.executeScript("arguments[0].removeAttribute('" + attributeRemove + "');", find(driver, xpathElement));
+	}
 	
 }
